d090ea69d57bc95a2c43106dd9b414ad0c35dfac
Nayana D Doyijode <ddoyijodenayana@gmail.com>
2025-07-07 20:07:41 +0530

    fix: created footer component

---
+++ project/student-app/src/Footer.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './Footer.css';
+import  Home from './home.js';
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+        <p>© 2025 Your Company. All rights reserved.</p>
+        </footer>
+     
+    </div>
+  );
+}
+
+export default Footer;
