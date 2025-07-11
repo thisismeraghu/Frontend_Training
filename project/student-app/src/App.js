b6f94b5ec7133bd2fc586f8bfad8dbba30b68472
Raghu <raghuisavailable@outlook.com>
2025-07-11 10:40:20 +0530

    fix: error code

--- project/student-app/src/App.js
+++ project/student-app/src/App.js
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import  Home from './home.js';
-=======
 import React from 'react';
 import './App.css'; // Import global styles
 
-import Header from './Component/Header/Header';
-import Footer from './Component/Footer/Footer';
->>>>>>> 54dc02558ebf71ff449efd6805662dadd00ce0ac
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-      </ul>
-    </div>
-=======
-    <Header />
-
-    <Footer />
+      Raghu
    </div>
->>>>>>> 54dc02558ebf71ff449efd6805662dadd00ce0ac
   );
 }
 
