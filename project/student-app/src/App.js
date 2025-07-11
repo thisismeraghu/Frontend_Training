bf75edd4bcea1411e245b0add0d39925719592ce
Nayana D Doyijode <ddoyijodenayana@gmail.com>
2025-07-11 19:32:51 +0530

    fix: created homepage

--- project/student-app/src/App.js
+++ project/student-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'; // Import global styles
+import HomePage from './HomePage/HomePage.js';
 
 
 function App() {
   return (
     <div>
-      Raghu
-   </div>
+      nayana
+    <HomePage/>
+    </div>
   );
 }
 
---
+++ project/student-app/src/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css'; // Import global styles
+
+function HomePage() {
+  return (
+    <div>
+    This is my home page
+    </div>
+  );
+}
+
+export default HomePage;
